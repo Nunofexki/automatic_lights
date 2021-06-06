ecd006da690efb1b2fca278aa914508ff377772a
nunofexki <nunofexki16@gmail.com>
2021-06-06 23:08:08 +0100

    Added Raspberry Pi code

---
+++ Raspberry/firebase-communication.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import serial
+import time
+import pyrebase
+
+config = {
+  "apiKey": "WSZY4ZPu6odDZUdi9ZeK6YPZ5oZuHicrHcibOeYK",
+  "authDomain": "automatic-lights1.firebaseapp.com",
+  "databaseURL": "https://automatic-lights1-default-rtdb.europe-west1.firebasedatabase.app",
+  "storageBucket": "automatic-lights1.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+if __name__ == '__main__':
+
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
+
+    while True:
+
+      status = db.child("Status").get().val()
+      
+      ser.write(str(status).encode('utf-8'))
+      
+      print("PYTHON - Status = " + str(status))
+
+      if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print("Read Line: " + line)
+            
+      time.sleep(1)      
